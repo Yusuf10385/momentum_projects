b3596e96b63f637bbd8998063955619e82b3542a
Yusuf10385 <yusuf.abore@gmail.com>
2020-06-17 14:27:09 +0100

    final

--- week_6/main.js
+++ week_6/main.js
@@ -1,4 +1,15 @@
-
-
-
-<script src="main.js"></script>
+/*
+Todo list
+*/
+window.onload = function (){
+    //variables
+    let form = document.getElementById("form");
+    let input = document.getElementById("input");
+    let btn = document.getElementById("btn");
+    let list = document.getElementById("list");
+    let btnClr = document.getElementById("btnClr");
+    let id = 1;
+    //listitem = {item: "todo item, checked: false"}
+    let liItem = "";
+    let todoList = [];
+}
